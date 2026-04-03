32c4a04d7ab9784167b86019c607bcf9607cf46a
sloiacan <sloiacan@pratt.edu>
2026-04-03 11:20:25 -0400

    Walking Animation...Kinda

--- MartianGame/Assets/Scripts/PlayerMovement/Movement.cs
+++ MartianGame/Assets/Scripts/PlayerMovement/Movement.cs
@@ -3,13 +3,15 @@ using UnityEngine.InputSystem; // Required for the new system
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 0.5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    public Animator animator;  
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Called via the Player Input component (Message: OnMove)
@@ -18,9 +20,61 @@ public class Movement : MonoBehaviour
         moveInput = value.Get<Vector2>(); // Captures WASD/Joystick input
     }
 
-    void FixedUpdate()
+    void Update()
     {
         // Apply velocity based on input and speed
-        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        //rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        //    rb.linearVelocity = moveInput * moveSpeed;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        transform.Translate(moveInput);
+
+        if(inputX != 0)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+        else if (inputX == 0) 
+        {
+
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+
+        if (inputY != 0)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+        else if (inputX == 0)
+        {
+
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+
     }
+    /*public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+      
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }*/
 }
+    
+
